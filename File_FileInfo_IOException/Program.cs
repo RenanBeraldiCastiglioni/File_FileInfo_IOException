3c13a3aa7fe6cda73546168f0f91db7865ce42b3
Renan Beraldi <beraldi2019renan@gmail.com>
2020-09-22 13:59:43 -0300

    Program Ended

---
+++ File_FileInfo_IOException/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace File_FileInfo_IOException
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath = @"c:\Windows\Temp\file1.txt";
+            string targetPath = @"c:\Windows\Temp\file2.txt";
+
+            try
+            {
+                FileInfo fileInfo = new FileInfo(sourcePath);
+                fileInfo.CopyTo(targetPath);
+                string[] lines = File.ReadAllLines(sourcePath);
+                foreach (string line in lines)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
